3a7c3702539fdc6a0244df4fd1508cb4bce30f74
Henrique Sturlini <hsturlini@usp.br>
2025-08-14 15:28:10 -0300

    Começando as classes

---
+++ libraries/ciclo/Resistivo.hpp
@@ -0,0 +1,24 @@
+#ifndef __RESISTIVO_HPP__
+#define __RESISTIVO_HPP__
+
+
+#include <SimpleTimer.h>
+#include <Arduino.h>
+#include "Encoder.hpp"
+#include "config.hpp"
+#include "Memory.hpp"
+#include "PID.hpp"
+#include "H_bridge_controller.hpp"
+#include <LiquidCrystal_I2C.h>
+
+
+class resistivo {
+
+    private:
+
+
+    public:
+
+
+
+};

21a246177f48986034fe7c68a8a436ffbcf2561d
HMHirano <heitor.massaru@gmail.com>
2025-08-25 09:17:57 -0300

    raw string literal e botão do download do csv

--- libraries/ciclo/Website.cpp
+++ libraries/ciclo/Website.cpp
@@ -60,94 +60,99 @@ String WEBSITE::websiteChart(){
 
 String WEBSITE::websiteGRAFICOS(){
     
-    String html = 
-"<!DOCTYPE html>\n"
-"<html lang='en'>\n"
-"<head>\n"
-"    <meta charset='UTF-8'>\n"
-"    <meta name='viewport' content='width=device-width, initial-scale=1.0'>\n"
-"    <title>Document</title>\n"
-"    <style>\n"
-"        body {\n"
-"            margin: 100px;\n"
-"        }\n"
-"    </style>\n"
-"</head>\n"
-"<body>\n"
-"\n"
-"    <script>\n"
-"        let params =  new URLSearchParams(document.location.search);\n"
-"        let id = params.get('id');\n"
-"        let modo = params.get('modo')\n"
-"        console.log('id: ' + id);\n"
-"    </script>\n"
-"\n"
-"    <h1 id='titulo'></h1>\n"
-"\n"
-"    <div>\n"
-"        <canvas id='myChart' style='width: 700px; display: block; height: 400px'></canvas>\n"
-"    </div>\n"
-"    <script src='https://cdn.jsdelivr.net/npm/chart.js'></script>\n"
-"    <button id='download'> Download </button>\n"
-"\n"
-"    <script>\n"
-"        const ctx = document.getElementById('myChart');\n"
-"\n"
-"        const url = '/data/' + modo + '/sessions';\n"
-"        let titulo_cont = 'Gráfico de torque x tempo da Sessão ' + id\n"
-"        document.getElementById('titulo').innerText = titulo_cont\n"
-"\n"
-"        const options = {\n"
-"            method: 'GET'\n"
-"        };\n"
-"\n"
-"        let chart; // Variável para armazenar a instância do gráfico\n"
-"\n"    
-"        fetch(url + '?id=' + id, options)\n"
-"            .then(response => response.json())\n"
-"            .then(response => {\n"
-"                console.log('fetch do Data');\n"
-"                console.log(response);\n"
-"                let tempo = response.tempo;\n"
-"                let torque = response.torque;\n"
-"                chart = createChart(tempo, torque); // Atribuindo o gráfico à variável\n"
-"            });\n"
-"\n"
-"        function createChart(tempo, torque) {\n"
-"            const data = {\n"
-"                labels: tempo,\n"
-"                datasets: [{\n"
-"                    label: 'Teste',\n"
-"                    data: torque,\n"
-"                    fill: false,\n"
-"                    borderColor: 'rgb(75, 192, 192)',\n"
-"                    tension: 0.1\n"
-"                }]\n"
-"            };\n"
-"            const config = {\n"
-"                type: 'line',\n"
-"                data: data,\n"
-"            };\n"
-"\n"
-"            return new Chart(ctx, config); // Retorna a instância do gráfico\n"
-"        }\n"
-"\n"
-"        function download() {\n"
-"            if (chart) {\n"
-"                const imageUrl = chart.toBase64Image();\n"
-"                const link = document.createElement('a');\n"
-"                link.href = imageUrl;\n"
-"                link.download = 'chart.png';\n"
-"                link.click();\n"
-"            } else {\n"
-"                alert('Chart not loaded yet.');\n"
-"            }\n"
-"        };\n"
-"\n"
-"        document.getElementById('download').addEventListener('click', download);\n"
-"    </script>\n"
-"</body>\n"
-"</html>\n";
+    String html = R"DELIM(
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <title>Document</title>
+    <style>
+        body {
+            margin: 100px;
+        }
+    </style>
+</head>
+<body>
+
+    <script>
+        let params =  new URLSearchParams(document.location.search);
+        let id = params.get('id');
+        let modo = params.get('modo')
+        console.log('id: ' + id);
+    </script>
+
+    <h1 id='titulo'></h1>
+
+    <div>
+        <canvas id='myChart' style='width: 700px; display: block; height: 400px'></canvas>
+    </div>
+    <script src='https://cdn.jsdelivr.net/npm/chart.js'></script>
+    <button id='grafico'> Download grafico </button>
+    <button id='csv'> Download csv </button>
+
+    <script>
+        const ctx = document.getElementById('myChart');
+
+        const url = '/data/' + modo + '/sessions';
+        let titulo_cont = 'Gráfico de torque x tempo da Sessão ' + id
+        document.getElementById('titulo').innerText = titulo_cont
+
+        const options = {
+            method: 'GET'
+        };
+
+        let chart; // Variável para armazenar a instância do gráfico
+\n 
+        fetch(url + '?id=' + id, options)
+            .then(response => response.json())
+            .then(response => {
+                console.log('fetch do Data');
+                console.log(response);
+                let tempo = response.tempo;
+                let torque = response.torque;
+                chart = createChart(tempo, torque); // Atribuindo o gráfico à variável
+            });
+
+        function createChart(tempo, torque) {
+            const data = {
+                labels: tempo,
+                datasets: [{
+                    label: 'Teste',
+                    data: torque,
+                    fill: false,
+                    borderColor: 'rgb(75, 192, 192)',
+                    tension: 0.1
+                }]
+            };
+            const config = {
+                type: 'line',
+                data: data,
+            };
+
+            return new Chart(ctx, config); // Retorna a instância do gráfico
+        }
+
+        function downloadGrafico() {
+            if (chart) {
+                const imageUrl = chart.toBase64Image();
+                const link = document.createElement('a');
+                link.href = imageUrl;
+                link.download = 'chart.png';
+                link.click();
+            } else {
+                alert('Chart not loaded yet.');
+            }
+        };
+
+        function download_csv() {}
+
+        document.getElementById('grafico').addEventListener('click', downloadGrafico);
+        document.getElementById('csv').addEventListener('click'), 
+    </script>
+</body>
+</html>
+)DELIM";
 
     return html;
 }
@@ -295,137 +300,116 @@ return html;
 
 String WEBSITE::websiteNormal(){
     //html
-String html = 
-"<!DOCTYPE html>\n"
-"<html lang='en'>\n"
-"<head>\n"
-    "<meta charset='UTF-8'>\n"  
-    "<meta name='viewport' content='width=device-width, initial-scale=1.0'>\n"
-"<title>Document</title>\n"
-"</head>\n"
-"<body>\n"
-    "<nav>\n"
-        "<ul>\n"
-            "<li class='liRes'><a style='color: #E0B700;' href='/resistivo/sessions'>Resistivo</a></li>\n"
-            "<li class='liNor'><a style='color:#0031A3;' href='/normal/sessions'>Normal</a></li>\n"
-            "<li class='liPas'><a style='color: #00B800;' href='/passivo/sessions'>Passivo</a></li>\n"
-        "</ul>\n"
-    "</nav>\n"
-    "<div id='ContainerBotoes'>\n"
-
-    "</div>\n"
-"</body>\n"
-"</html>\n"
-
-
-
-"<style>\n"
-
-"body{\n"
-"    padding: 0;\n"
-"    margin: 0;\n"
-"    background-color: #EBF1FF;\n"
-"}\n"
-
-"nav ul{\n"
-"    list-style-type: none;\n"
-"    display: grid;\n"
-"    grid-template-columns: repeat(3, 1fr);\n"
-"    padding: 0;\n"
-"    margin: 0;\n"
-"    height: 60px;\n"
-"}\n"
-
-"nav ul li{\n"
-"    text-align: center;\n"
-"    justify-content: center;\n"
-"    display: flex;\n"
-"    align-items: center;\n"
-"}\n"
-
-".liRes{\n"
-"    background-color: #FFF7D6;\n"
-"    border: 1px solid black;\n"
-"}\n"
-
-".liNor{\n"
-"    background-color: #EBF1FF;\n"
-"    border: 1px solid black;\n"
-"    border-bottom: none;\n"
-"    text-decoration: underline;\n"
-"    text-decoration-color: #0031A3;\n"
-"}\n"
-
-".liPas{\n"
-"    background-color: #EBFFEB;\n"
-"    border: 1px solid black;\n"
-"}\n"
-
-"nav ul li a{\n"
-"    text-decoration: none;\n"
-"    font-size: 22px;\n"
-"}\n"
-
-"div{\n"
-"    display: flex;\n"
-"    justify-content: space-between;\n"
-"    flex-direction: row;\n"
-"    width: 350px;\n"
-"    margin-left: auto;\n"
-"    margin-right: auto;\n"
-"    margin-top: 50px;\n"
-"    gap: 12px;\n"
-"    flex-wrap: wrap;\n"
-"}\n"
-
-"div a{\n"
-"    flex: 0 0 40%;\n"
-"}\n"
-
-"div a button{\n"
-"    height: 50px;\n"
-"    border-radius: 14px;\n"
-"    cursor: pointer;\n"
-"    width: 100px;\n"
-"    background-color: white;\n"
-"    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;\n"
-"    border: 1px solid black;\n"
-"}\n"
-
-"div a button:hover{\n"
-"    border: 1.5px solid black;\n"
-"}\n"
-
-"</style>\n"
-
-"<script>\n"
-
-"const url = '/number/normal/sessions';\n"
-"fetch(url).then(response =>{\n"
-"    return response.json();\n"
-"})\n"
-".then(data => {\n"
-"    nbotoes = data.sessions;\n"
-"    const a = document.createElement('a');\n"
-"    const Botao = document.createElement('button');\n"
-"    const ContainerBotoes = document.getElementById('ContainerBotoes');\n"
-"    for (let index = 1; index <= nbotoes; index++) {\n"
-"        const a = document.createElement('a');\n"
-"        const Botao = document.createElement('button');\n"
-"        a.href = '/graficos' + '?id=' + index + '&' + 'modo=normal';\n" 
-"        Botao.innerHTML = 'Sessao ' + index;\n"
-"        a.appendChild(Botao);\n"
-"        ContainerBotoes.appendChild(a);\n"
-"    }\n"
-"})\n"
-".catch(error =>{\n"
-"    console.log('Erro ao buscar os dados:',error);\n"
-"});\n"
-
-
-
-"</script>\n";
-
+String html = R"DELIM(
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+   "<meta charset='UTF-8'>\n
+   "<meta name='viewport' content='width=device-width, initial-scale=1.0'>
+<title>Document</title>
+</head>
+<body>
+   "<nav>
+       "<ul>
+           "<li class='liRes'><a style='color: #E0B700;' href='/resistivo/sessions'>Resistivo</a></li>
+           "<li class='liNor'><a style='color:#0031A3;' href='/normal/sessions'>Normal</a></li>
+           "<li class='liPas'><a style='color: #00B800;' href='/passivo/sessions'>Passivo</a></li>
+       "</ul>
+   "</nav>
+   "<div id='ContainerBotoes'>
+   "</div>
+</body>
+</html>
+<style>
+body{
+    padding: 0;
+    margin: 0;
+    background-color: #EBF1FF;
+}
+nav ul{
+    list-style-type: none;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    padding: 0;
+    margin: 0;
+    height: 60px;
+}
+nav ul li{
+    text-align: center;
+    justify-content: center;
+    display: flex;
+    align-items: center;
+}
+.liRes{
+    background-color: #FFF7D6;
+    border: 1px solid black;
+}
+.liNor{
+    background-color: #EBF1FF;
+    border: 1px solid black;
+    border-bottom: none;
+    text-decoration: underline;
+    text-decoration-color: #0031A3;
+}
+.liPas{
+    background-color: #EBFFEB;
+    border: 1px solid black;
+}
+nav ul li a{
+    text-decoration: none;
+    font-size: 22px;
+}
+div{
+    display: flex;
+    justify-content: space-between;
+    flex-direction: row;
+    width: 350px;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 50px;
+    gap: 12px;
+    flex-wrap: wrap;
+}
+div a{
+    flex: 0 0 40%;
+}
+div a button{
+    height: 50px;
+    border-radius: 14px;
+    cursor: pointer;
+    width: 100px;
+    background-color: white;
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+    border: 1px solid black;
+}
+div a button:hover{
+    border: 1.5px solid black;
+}
+</style>
+<script>
+const url = '/number/normal/sessions';
+fetch(url).then(response =>{
+    return response.json();
+})
+.then(data => {
+    nbotoes = data.sessions;
+    const a = document.createElement('a');
+    const Botao = document.createElement('button');
+    const ContainerBotoes = document.getElementById('ContainerBotoes');
+    for (let index = 1; index <= nbotoes; index++) {
+        const a = document.createElement('a');
+        const Botao = document.createElement('button');
+        a.href = '/graficos' + '?id=' + index + '&' + 'modo=normal';\
+        Botao.innerHTML = 'Sessao ' + index;
+        a.appendChild(Botao);
+        ContainerBotoes.appendChild(a);
+    }
+})
+.catch(error =>{
+    console.log('Erro ao buscar os dados:',error);
+});
+</script>
+)DELIM";
 
 return html;
 
@@ -434,137 +418,120 @@ return html;
 
 String WEBSITE::websitePassivo(){
     //html
-String html = 
-"<!DOCTYPE html>\n"
-"<html lang='en'>\n"
-"<head>\n"
-    "<meta charset='UTF-8'>\n"  
-    "<meta name='viewport' content='width=device-width, initial-scale=1.0'>\n"
-"<title>Document</title>\n"
-"</head>\n"
-"<body>\n"
-    "<nav>\n"
-        "<ul>\n"
-            "<li class='liRes'><a style='color: #E0B700;' href='/resistivo/sessions'>Resistivo</a></li>\n"
-            "<li class='liNor'><a style='color:#0031A3;' href='/normal/sessions'>Normal</a></li>\n"
-            "<li class='liPas'><a style='color: #00B800;' href='/passivo/sessions'>Passivo</a></li>\n"
-        "</ul>\n"
-    "</nav>\n"
-    "<div id='ContainerBotoes'>\n"
-
-    "</div>\n"
-"</body>\n"
-"</html>\n"
-
-
-
-"<style>\n"
-
-"body{\n"
-"    padding: 0;\n"
-"    margin: 0;\n"
-"    background-color: #EBFFEB;\n"
-"}\n"
-
-"nav ul{\n"
-"    list-style-type: none;\n"
-"    display: grid;\n"
-"    grid-template-columns: repeat(3, 1fr);\n"
-"    padding: 0;\n"
-"    margin: 0;\n"
-"    height: 60px;\n"
-"}\n"
-
-"nav ul li{\n"
-"    text-align: center;\n"
-"    justify-content: center;\n"
-"    display: flex;\n"
-"    align-items: center;\n"
-"}\n"
-
-".liRes{\n"
-"    background-color: #FFF7D6;\n"
-"    border: 1px solid black;\n"
-"}\n"
-
-".liNor{\n"
-"    background-color: #EBF1FF;\n"
-"    border: 1px solid black;\n"
-"}\n"
-
-".liPas{\n"
-"    background-color: #EBFFEB;\n"
-"    border: 1px solid black;\n"
-"    border-bottom: none;\n"
-"    border-right: none;\n"
-"    text-decoration: underline;\n"
-"    text-decoration-color: #00B800;\n"
-"}\n"
-
-"nav ul li a{\n"
-"    text-decoration: none;\n"
-"    font-size: 22px;\n"
-"}\n"
-
-"div{\n"
-"    display: flex;\n"
-"    justify-content: space-between;\n"
-"    flex-direction: row;\n"
-"    width: 350px;\n"
-"    margin-left: auto;\n"
-"    margin-right: auto;\n"
-"    margin-top: 50px;\n"
-"    gap: 12px;\n"
-"    flex-wrap: wrap;\n"
-"}\n"
-
-"div a{\n"
-"    flex: 0 0 40%;\n"
-"}\n"
-
-"div a button{\n"
-"    height: 50px;\n"
-"    border-radius: 14px;\n"
-"    cursor: pointer;\n"
-"    width: 100px;\n"
-"    background-color: white;\n"
-"    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;\n"
-"    border: 1px solid black;\n"
-"}\n"
-
-"div a button:hover{\n"
-"    border: 1.5px solid black;\n"
-"}\n"
-
-"</style>\n"
-
-"<script>\n"
-
-"const url = '/number/passivo/sessions';\n"
-"fetch(url).then(response =>{\n"
-"    return response.json();\n"
-"})\n"
-".then(data => {\n"
-"    nbotoes = data.sessions;\n"
-"    const a = document.createElement('a');\n"
-"    const Botao = document.createElement('button');\n"
-"    const ContainerBotoes = document.getElementById('ContainerBotoes');\n"
-"    for (let index = 1; index <= nbotoes; index++) {\n"
-"        const a = document.createElement('a');\n"
-"        const Botao = document.createElement('button');\n"
-"        a.href = '/graficos' + '?id=' + index + '&' + 'modo=passivo';\n"
-"        Botao.innerHTML = 'Sessao ' + index;\n"
-"        a.appendChild(Botao);\n"
-"        ContainerBotoes.appendChild(a);\n"
-"    }\n"
-"})\n"
-".catch(error =>{\n"
-"    console.log('Erro ao buscar os dados:',error);\n"
-"});\n"
+String html = R"DELIM(
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+   "<meta charset='UTF-8'>
+   "<meta name='viewport' content='width=device-width, initial-scale=1.0'>
+<title>Document</title>
+</head>
+<body>
+   "<nav>
+       "<ul>
+           "<li class='liRes'><a style='color: #E0B700;' href='/resistivo/sessions'>Resistivo</a></li>
+           "<li class='liNor'><a style='color:#0031A3;' href='/normal/sessions'>Normal</a></li>
+           "<li class='liPas'><a style='color: #00B800;' href='/passivo/sessions'>Passivo</a></li>
+       "</ul>
+   "</nav>
+   "<div id='ContainerBotoes'>
+   "</div>
+</body>
+</html>
+<style>
+body{
+    padding: 0;
+    margin: 0;
+    background-color: #EBFFEB;
+}
+nav ul{
+    list-style-type: none;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    padding: 0;
+    margin: 0;
+    height: 60px;
+}
+nav ul li{
+    text-align: center;
+    justify-content: center;
+    display: flex;
+    align-items: center;
+}
+.liRes{
+    background-color: #FFF7D6;
+    border: 1px solid black;
+}
+.liNor{
+    background-color: #EBF1FF;
+    border: 1px solid black;
+}
+.liPas{
+    background-color: #EBFFEB;
+    border: 1px solid black;
+    border-bottom: none;
+    border-right: none;
+    text-decoration: underline;
+    text-decoration-color: #00B800;
+}
+nav ul li a{
+    text-decoration: none;
+    font-size: 22px;
+}
+div{
+    display: flex;
+    justify-content: space-between;
+    flex-direction: row;
+    width: 350px;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 50px;
+    gap: 12px;
+    flex-wrap: wrap;
+}
+div a{
+    flex: 0 0 40%;
+}
+div a button{
+    height: 50px;
+    border-radius: 14px;
+    cursor: pointer;
+    width: 100px;
+    background-color: white;
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+    border: 1px solid black;
+}
+div a button:hover{
+    border: 1.5px solid black;
+}
+</style>
+<script>
+const url = '/number/passivo/sessions';
+fetch(url).then(response =>{
+    return response.json();
+})
+.then(data => {
+    nbotoes = data.sessions;
+    const a = document.createElement('a');
+    const Botao = document.createElement('button');
+    const ContainerBotoes = document.getElementById('ContainerBotoes');
+    for (let index = 1; index <= nbotoes; index++) {
+        const a = document.createElement('a');
+        const Botao = document.createElement('button');
+        a.href = '/graficos' + '?id=' + index + '&' + 'modo=passivo';
+        Botao.innerHTML = 'Sessao ' + index;
+        a.appendChild(Botao);
+        ContainerBotoes.appendChild(a);
+    }
+})
+.catch(error =>{
+    console.log('Erro ao buscar os dados:',error);
+});
 
 
 
-"</script>\n";
+</script>
+)DELIM";
 
 
 return html;
